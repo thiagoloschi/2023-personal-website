9939f78b9900c2dfb78c4e3f740de29ec813bc95
Thiago Loschi <thiagoloschi@thiagos-mbp-2.lan>
2023-09-22 17:01:50 -0400

    Add Google analytcs

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -13,7 +13,6 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    // "gatsby-plugin-google-gtag"
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
@@ -39,6 +38,35 @@ const config: GatsbyConfig = {
     options: {
       spaceId: process.env.CONTENTFUL_SPACE,
       accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      trackingIds: [
+        process.env.GOOGLE_TAG,
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIds
+      gtagConfig: {
+        // optimize_id: "OPT_CONTAINER_ID",
+        // anonymize_ip: true,
+        send_page_view: true,
+        cookie_expires: 0,
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // Setting this parameter is also optional
+        // respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Defaults to https://www.googletagmanager.com
+        // origin: "https://thiagoloschi.com",
+        // Delays processing pageview events on route update (in milliseconds)
+        delayOnRouteUpdate: 0,
+      },
     },
   },
   `gatsby-plugin-image`]
