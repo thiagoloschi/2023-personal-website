55303e4bd8914296808e25945d5676b48848b917
Thiago Loschi <thiagoloschi@thiagos-mbp-2.lan>
2023-09-22 15:34:48 -0400

    Fix Recommendation section reponsiveness

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export {ExperienceCard} from './ExperienceCard/ExperienceCard';
 export {ExperienceSection} from './ExperienceSection/ExperienceSection';
 export {ReferralSection} from './ReferralSection/ReferralSection';
 export {ContactSection} from './ContactSection/ContactSection';
+export {Footer} from './Footer/Footer';
