70ed5dfbf5857c1324d5e2920eed2918abfe9ca4
Thiago Loschi <thiagoloschi@gmail.com>
2023-09-24 16:32:33 -0400

    Add hover effects to Experience card

--- src/components/ExperienceCard/ExperienceCard.tsx
+++ src/components/ExperienceCard/ExperienceCard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ExperienceCard.scss";
 import { type Experience } from "../../types";
+import { OutboundLink } from "gatsby-plugin-google-gtag";
 
 interface Props {
   experience: Experience;
@@ -8,14 +9,16 @@ interface Props {
 
 export function ExperienceCard({ experience }: Props) {
   return (
-    <div className="ExperienceCard" style={{backgroundColor: experience.color, color: experience.text}}>
+    <div className="ExperienceCard" style={{backgroundColor: experience.color, color: experience.text}}> 
       <h2 className="Year">{experience.year}</h2>
       <p>
         {experience.role}
       </p>
-      <div className="ImageWrapper">
-        <img src={experience.image} />
-      </div>
+        <OutboundLink className="CompanyLink" href={experience.url} target="_blank" title={`Visit ${experience.company}'s website`}> 
+          <div className="ImageWrapper">
+            <img src={experience.image} />
+          </div>
+        </OutboundLink>
       <p>{experience.description}</p>
     </div>
   );
