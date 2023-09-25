96f1e88153ff929c041aad1e0b5739cba286c8cc
Thiago Loschi <thiagoloschi@gmail.com>
2023-09-25 17:04:44 -0400

    Clean up the Contact section

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { OutboundLink } from "gatsby-plugin-google-gtag";
 import {type ContactInfo} from "../../types";
-import "./ContactSection.scss"
+import "./Contact.scss"
 
 interface Props {
   contactInfo: ContactInfo[];
 }
 
-export function ContactSection({contactInfo}: Props) {
+export function Contact({contactInfo}: Props) {
   const contactInfoMap = contactInfo.map((info) => (
     <OutboundLink className="ContactLink" key={info.name} href={info.url} target="_blank" title={`Visit my ${info.name} profile`}>
       <i className={`Icon fa-brands fa-${info.name}`} />
@@ -15,7 +15,7 @@ export function ContactSection({contactInfo}: Props) {
   ));
     
   return (
-    <div className="ContactSection">
+    <div className="Contact">
       <div className="ContactMap">
         {contactInfoMap}
       </div>
