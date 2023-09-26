7fd5a8dbdbd4861d54b547360ed0b0ce9b2f5fdd
Thiago Loschi <thiagoloschi@gmail.com>
2023-09-25 20:49:42 -0400

    Removes images folder ref

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,21 +5,11 @@ require("dotenv").config({
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `ThiagoSPersonalWebsite`,
+    title: 'Thiago Loschi',
     siteUrl: `https://www.thiagoloschi.com`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
@@ -49,27 +39,15 @@ const config: GatsbyConfig = {
       // This object gets passed directly to the gtag config command
       // This config will be shared across all trackingIds
       gtagConfig: {
-        // optimize_id: "OPT_CONTAINER_ID",
-        // anonymize_ip: true,
-        send_page_view: true,
         cookie_expires: 0,
       },
       // This object is used for configuration specific to this plugin
       pluginConfig: {
         // Puts tracking script in the head instead of the body
         head: true,
-        // Setting this parameter is also optional
-        // respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        // exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Defaults to https://www.googletagmanager.com
-        // origin: "https://thiagoloschi.com",
-        // Delays processing pageview events on route update (in milliseconds)
-        // delayOnRouteUpdate: 0,
       },
     },
-  },
-  `gatsby-plugin-image`]
+  }]
 };
 
 export default config;
